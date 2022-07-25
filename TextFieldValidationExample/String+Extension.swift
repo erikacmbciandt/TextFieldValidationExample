2e92b9b2d45ea218666272d0a3e7d6838b3a156a
Erika Bueno <erikab@ciandt.com>
2022-07-25 15:37:32 -0300

    first commit

---
+++ TextFieldValidationExample/String+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  String+Extension.swift
+//  TextFieldValidationExample
+//
+//  Created by Erika C. Matesz Bueno on 25/07/22.
+//
+
+import Foundation
+
+extension String {
+    var isEmail: Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: self)
+    }
+}
--- TextFieldValidationExample/ViewController.swift
+++ TextFieldValidationExample/ViewController.swift
@@ -9,11 +9,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Outlets
+
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var button: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setup()
     }
 
+    // MARK: - UI Customization
+
+    // Initial state
+    private func setup() {
+        textField.layer.masksToBounds = true
+        textField.layer.borderWidth = 1
+        button.isEnabled = false
+        label.textColor = .red
+    }
+
+    // Empty state
+    private func displayAsEmpty() {
+        textField.layer.borderColor = UIColor.systemGray.cgColor
+        label.text = ""
+        button.isEnabled = false
+    }
+
+    // Valid state
+    private func displayAsValid() {
+        textField.layer.borderColor = UIColor.systemBlue.cgColor
+        label.text = ""
+        button.isEnabled = true
+    }
+
+    // Invalid state
+    private func displayAsInvalid() {
+        textField.layer.borderColor = UIColor.red.cgColor
+        label.text = "invalid email"
+        button.isEnabled = false
+    }
+
+    // MARK: - Validation Logic
+
+    @IBAction func textFieldChanged(_ sender: Any) {
+        if let textFieldValue = textField.text {
+            if textFieldValue.isEmpty {
+                displayAsEmpty()
+                return
+            }
+            textFieldValue.isEmail ? displayAsValid() : displayAsInvalid()
+        }
+    }
 
 }
-
